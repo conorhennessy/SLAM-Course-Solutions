d253c18963a1fc02cdcd0d16a4055272fc6b5e7d
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2020-07-10 19:01:55 +0100

    :heavy_plus_sign: :sparkles: Remove outliers by RANSAC & locate obj.
    Outliers are now removed by RANSAC and the book is now highlighted in a nice bounding box! :blue_book: :white_square_button:

--- book_SURFMatchFeatures.m
+++ book_SURFMatchFeatures.m
@@ -1,5 +1,10 @@
 %% Book Cover feature detection by SURF feature detection, extraction and matching
+% With RANSAC to filter out outliers. By est. of geometric transformation
+% and fundalmental matrix.
+% RANSAC; Random Sample Consensus. An iterative est. of parameters to a
+% mathamatical model from a set of observed data that contains outliers.
 % Refrence: https://uk.mathworks.com/videos/computer-vision-made-easy-81802.html
+
 close all;
 clc;
 
@@ -8,6 +13,13 @@ I1 = imread('LostBook3.jpg');
 figure; imshow(I1); title('Obj');
 I2 = imread('ClutteredScene3.jpg');
 figure; imshow(I2); title('Scene');
+
+% Define location of object in I1
+boxPolygon = [1, 1;...                           % top-left
+             size(I1, 2), 1;...                  % top-right
+             size(I1, 2), size(I1, 1);...        % bottom-right
+             1, size(I1, 1);...                  % bottom-left
+             1, 1];                               % top-left again to close the polygon
 
 %% Detect Features
 points1 = detectSURFFeatures(rgb2gray(I1));
@@ -26,5 +38,14 @@ title('Detected Features');
 index_pairs = matchFeatures(feats1, feats2, 'Prenormalized', true);
 matched_pts1 = validpts1(index_pairs(:, 1));
 matched_pts2 = validpts2(index_pairs(:, 2));
-figure; showMatchedFeatures(I1, I2, matched_pts1, matched_pts2, 'montage');
-title('Initial Matches');
+%% Remove outliers while est. geometric transform using RANSAC
+[transform, inlierPoints1, inlierPoints2] = estimateGeometricTransform(matched_pts1, matched_pts2, 'affine');
+figure; showMatchedFeatures(I1, I2, inlierPoints1, inlierPoints2, 'montage');
+title('Filtered Matches');
+
+%% Use est. transform to locate the obj.
+newBoxPolygon = transformPointsForward(transform, boxPolygon);
+figure; imshow(I2);
+hold on; 
+line(newBoxPolygon(:, 1), newBoxPolygon(:, 2), 'Color', 'g', 'LineWidth', 5);
+title('Detected obj.');
