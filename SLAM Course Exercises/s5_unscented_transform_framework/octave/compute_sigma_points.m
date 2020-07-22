3b6086ddbc19c54d18c833af1dc9e352c37078df
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2020-07-22 15:07:20 +0100

    :sparkles: Innit of assignment 5 of Robot Mapping Course
    Topic: The Unscented Transform

---
+++ SLAM Course Exercises/s5_unscented_transform_framework/octave/compute_sigma_points.m
@@ -0,0 +1,19 @@
+function [sigma_points, w_m, w_c] = compute_sigma_points(mu, sigma, lambda, alpha, beta)
+% This function samples 2n+1 sigma points from the distribution given by mu and sigma
+% according to the unscented transform, where n is the dimensionality of mu.
+% Each column of sigma_points should represent one sigma point
+% i.e. sigma_points has a dimensionality of nx2n+1.
+% The corresponding weights w_m and w_c of the points are computed using lambda, alpha, and beta:
+% w_m = [w_m_0, ..., w_m_2n], w_c = [w_c_0, ..., w_c_2n] (i.e. each of size 1x2n+1)
+% They are later used to recover the mean and covariance respectively.
+
+n = length(mu);
+sigma_points = zeros(n,2*n+1);
+
+% TODO: compute all sigma points
+
+
+% TODO compute weight vectors w_m and w_c
+
+
+end
---
+++ SLAM Course Exercises/s5_unscented_transform_framework/octave/recover_gaussian.m
@@ -0,0 +1,16 @@
+function [mu, sigma] = recover_gaussian(sigma_points, w_m, w_c)
+% This function computes the recovered Gaussian distribution (mu and sigma)
+% given the sigma points (size: nx2n+1) and their weights w_m and w_c:
+% w_m = [w_m_0, ..., w_m_2n], w_c = [w_c_0, ..., w_c_2n].
+% The weight vectors are each 1x2n+1 in size,
+% where n is the dimensionality of the distribution.
+
+% Try to vectorize your operations as much as possible
+
+% TODO: compute mu
+
+
+% TODO: compute sigma
+
+
+end
