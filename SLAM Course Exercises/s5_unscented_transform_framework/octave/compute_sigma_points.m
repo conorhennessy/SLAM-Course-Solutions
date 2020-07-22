392d0223fe0c818b1a07bad28bc06a766a0a4d3c
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2020-07-22 18:36:30 +0100

    :heavy_plus_sign: :sparkles: Completion of assignment 5 of SLAM Course

--- SLAM Course Exercises/s5_unscented_transform_framework/octave/compute_sigma_points.m
+++ SLAM Course Exercises/s5_unscented_transform_framework/octave/compute_sigma_points.m
@@ -8,12 +8,33 @@ function [sigma_points, w_m, w_c] = compute_sigma_points(mu, sigma, lambda, alph
 % They are later used to recover the mean and covariance respectively.
 
 n = length(mu);
-sigma_points = zeros(n,2*n+1);
+sigma_points = zeros(n, 2*n+1);
+
+%init the weights
+w_m = zeros(1, 2*n+1);
+w_c = zeros(1, 2*n+1);
 
 % TODO: compute all sigma points
+% First sigma point is the mean
+sigma_points(:, 1) = mu;
+% For remaining sigma points
+% first, compute alternative to the square root matrix, the Cholesky decompositon by Cholesky matrix (sigma = L * L^T)
+cholesky_matrix = chol((n+lambda)*sigma, "lower");
+for i = 1:2n
+    if i <= n
+        sigma_points(:, i+1) = mu + cholesky_matrix(:, i);
+    endif
+    if i > n
+        sigma_points(:, i+1) = mu - cholesky_matrix(:, i);
+    endif
+endfor
 
-
-% TODO compute weight vectors w_m and w_c
-
+% TODO compute weight vectors w_m mean and w_c covariance
+% Weights for first sigma value
+w_m(1,1) = lambda / (n + lambda);
+w_c(1, 1) = w_m(1, 1) + (1 - power(alpha, 2) + beta);
+% Weights for remaining sigma values
+w_m(1, 2:end) = 1 / (2 * (n + lambda));
+w_c(1, 2:end) = w_m(1, 2:end);
 
 end
--- SLAM Course Exercises/s5_unscented_transform_framework/octave/recover_gaussian.m
+++ SLAM Course Exercises/s5_unscented_transform_framework/octave/recover_gaussian.m
@@ -7,10 +7,21 @@ function [mu, sigma] = recover_gaussian(sigma_points, w_m, w_c)
 
 % Try to vectorize your operations as much as possible
 
-% TODO: compute mu
+n = size(sigma_points, 1);
 
+% init mean (mu) and covariance (sigma)
+mu = zeros(n, 1);
+sigma = zeros(n, n);
+
+% Compute the mean (mu) and covariance (sigma) of the resulting distribution given the transformed sigma points and corresponding weights.
+% TODO: compute mu
+for i = 1:2*n+1
+    mu = mu + w_m(1, i) * sigma_points(:, i);
+endfor
 
 % TODO: compute sigma
-
+for i = 1:2*n+1
+    sigma = sigma + w_c(1, i) * (sigma_points(:, i) - mu) * (sigma_points(:, i) - mu)';
+endfor
 
 end
