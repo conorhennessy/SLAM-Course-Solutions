29b57de876aef9c4bfe4ed6b471463877b842b74
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2020-08-10 15:37:54 +0100

    :heavy_plus_sign: :sparkles: Completion of assignment 7, exercise 2 of SLAM Course
    You may need to do a `pkg load statistics` to ensure all packages are installed. This is needed for normrnd & unifrnd functions .
    
    I'm soooo chuffed this is working! A good days work!

--- s7_ex2_pf/octave/prediction_step.m
+++ s7_ex2_pf/octave/prediction_step.m
@@ -15,12 +15,26 @@ r2Noise = noise(3);
 numParticles = length(particles);
 
 for i = 1:numParticles
-
   % append the old position to the history of the particle
   particles(i).history{end+1} = particles(i).pose;
 
   % TODO: sample a new pose for the particle
-  
+
+  % First update the robot particles according to the motion model
+  % By computing the new mu based on the noise-free (odometry-based) motion model
+  %mu(1) = particles(i).pose + (u.t * cos(particles(i).pose(3) + u.r1));
+  %mu(2) = particles(i).pose + (u.t * sin(particles(i).pose(3) + u.r1));
+  %mu(3) = particles(i).pose + (u.r1 + u.r2);
+  mu = particles(i).pose + [u.t * cos(particles(i).pose(3) + u.r1); u.t * sin(particles(i).pose(3) + u.r1); u.r1 + u.r2];
+  mu(3) = normalize_angle(mu(3));
+
+  % Then, update particles(i) according to the motion represented by u and the noise
+  %%  Note: The function normrnd(µ, σ) allows to draw samples from a Gaussian with
+  %%  mean µ and standard deviation σ. Where µ is mu & σ is standard deviation
+  %%  Standard deviation is given by the sum of noise parameters as required.
+  particles(i).pose(1) = normrnd(mu(1), r1Noise + transNoise + r2Noise);
+  particles(i).pose(2) = normrnd(mu(2), r1Noise + transNoise + r2Noise);
+  particles(i).pose(3) = normrnd(mu(3), r1Noise + r2Noise);
 
 end
 
--- s7_ex2_pf/octave/resample.m
+++ s7_ex2_pf/octave/resample.m
@@ -29,14 +29,27 @@ end
 newParticles = struct;
 
 % TODO: implement the low variance re-sampling
+% This corresponds to slide 18 of Particle Filter lecture. 26:00 in SLAM Course - 11 video
+%% Note: low variance re-sampling, also called Stochastic Universal Sampling
+%% O(J)
+%% This sampling is in linear time. If all samples all have the same weight,
+%% this guarentees we get the same samples every time. Thus low variance. 
 
 % the cumulative sum
+c = w(1);
 
 % initialize the step and the current position on the roulette wheel
+r = rand(1) * numParticles;
+i = 1;
 
 % walk along the wheel to select the particles
-for i = 1:numParticles
-  
-end
+for j = 1:newParticles
+  U = r + (j-1) * inv(newParticles);
+  while U > c
+    i = i + 1;
+    c = c + w(i);
+  endwhile
+  newParticles(j) = newParticles(i);
+endfor
 
 end
