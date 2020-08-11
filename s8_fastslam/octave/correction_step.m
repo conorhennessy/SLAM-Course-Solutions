529131742fbe0bfe374c8401df05c51180c567ba
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2020-08-11 13:55:43 +0100

    :heavy_plus_sign: :sparkles: Completion of assignment 8 of SLAM Course
    You may need to do a `pkg load statistics` to ensure all packages are installed. This is needed for normrnd function in this implementation.

--- s8_fastslam/octave/correction_step.m
+++ s8_fastslam/octave/correction_step.m
@@ -14,6 +14,8 @@ numParticles = length(particles);
 m = size(z, 2);
 
 % TODO: Construct the sensor noise matrix Q_t (2 x 2)
+Q_t = [0.1, 0.0;
+       0.0, 0.1]; 
 
 % process each particle
 for i = 1:numParticles
@@ -32,11 +34,19 @@ for i = 1:numParticles
     if (particles(i).landmarks(l).observed == false)
 
       % TODO: Initialize its position based on the measurement and the current robot pose:
+      % See to line 37 of correction_step in s4_ekf_SLAM for how this was simarly done previously in EKF. 
+      robot_x = robot(1);
+      robot_y = robot(2);
+      robot_r = robot(3);
+      particles(i).landmarks(1).mu = [ robot_x + z(j).range * cos(normalize_angle(z(j).bearing + robot_r));
+                                       robot_y + z(j).range * sin(normalize_angle(z(j).bearing + robot_r))];
 
       % get the Jacobian with respect to the landmark position
       [h, H] = measurement_model(particles(i), z(j));
 
       % TODO: initialize the EKF for this landmark
+      % This corresponds to line 9 of slide 28 of FAST SLAM lecture (12)
+      particles(i).landmarks(l).sigma = inv(H) * Q_t * inv(H)';
 
       % Indicate that this landmark has been observed
       particles(i).landmarks(l).observed = true;
@@ -47,15 +57,27 @@ for i = 1:numParticles
       [expectedZ, H] = measurement_model(particles(i), z(j));
 
       % TODO: compute the measurement covariance
+      % This corresponds to line 14 of slide 30 of FAST SLAM lecture (12)
+      Q = (H * particles(i).landmarks(l).sigma * H') + Q_t;
 
       % TODO: calculate the Kalman gain
+      % This corresponds to line 15 of slide 30 of FAST SLAM lecture (12)
+      K = particles(i).landmarks(l).sigma * H' * inv(Q_t);
 
-      % TODO: compute the error between the z and expectedZ (remember to normalize the angle)
+      % TODO: compute the error between the z and expectedZ (remember to normalize the angle)'
+      % error for range and bearing
+      Z_diff = [z(j).range - expectedZ(1); normalize_angle(z(j).bearing - expectedZ(2))];
 
       % TODO: update the mean and covariance of the EKF for this landmark
+      % mean, this corresponds to line 16 of slide 30 of FAST SLAM lecture (12)
+      particles(i).landmarks(1).mu = particles(i).landmarks(1).mu + K * (Z_diff);
+      % covariance, this corresponds to line 17 of slide 30 of FAST SLAM lecture (12)
+      particles(i).landmarks(1).sigma = (eye(2) - K * H) * particles(i).landmarks(1).sigma;
 
       % TODO: compute the likelihood of this observation, multiply with the former weight
       %       to account for observing several features in one time step
+      % Weight, this corresponds to line 13 of slide 44 of FAST SLAM lecture (12)
+      particles(i).weight = 1/sqrt(det(2 * pi * Q)) * exp(-0.5 * (Z_diff)' * inv(Q) * Z_diff);
 
     end
 
