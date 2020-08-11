3e94fa7199cf56f8e81469e9deb54a824fc6b973
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2020-08-11 11:18:46 +0100

    :sparkles: Completion of assignment 8 of SLAM Course
    Topic: Fast SLAM

---
+++ s8_fastslam/octave/correction_step.m
@@ -0,0 +1,65 @@
+function particles = correction_step(particles, z)
+
+% Weight the particles according to the current map of the particle
+% and the landmark observations z.
+% z: struct array containing the landmark observations.
+% Each observation z(j) has an id z(j).id, a range z(j).range, and a bearing z(j).bearing
+% The vector observedLandmarks indicates which landmarks have been observed
+% at some point by the robot.
+
+% Number of particles
+numParticles = length(particles);
+
+% Number of measurements in this time step
+m = size(z, 2);
+
+% TODO: Construct the sensor noise matrix Q_t (2 x 2)
+
+% process each particle
+for i = 1:numParticles
+  robot = particles(i).pose;
+  % process each measurement
+  for j = 1:m
+    % Get the id of the landmark corresponding to the j-th observation
+    % particles(i).landmarks(l) is the EKF for this landmark
+    l = z(j).id;
+
+    % The (2x2) EKF of the landmark is given by
+    % its mean particles(i).landmarks(l).mu
+    % and by its covariance particles(i).landmarks(l).sigma
+
+    % If the landmark is observed for the first time:
+    if (particles(i).landmarks(l).observed == false)
+
+      % TODO: Initialize its position based on the measurement and the current robot pose:
+
+      % get the Jacobian with respect to the landmark position
+      [h, H] = measurement_model(particles(i), z(j));
+
+      % TODO: initialize the EKF for this landmark
+
+      % Indicate that this landmark has been observed
+      particles(i).landmarks(l).observed = true;
+
+    else
+
+      % get the expected measurement
+      [expectedZ, H] = measurement_model(particles(i), z(j));
+
+      % TODO: compute the measurement covariance
+
+      % TODO: calculate the Kalman gain
+
+      % TODO: compute the error between the z and expectedZ (remember to normalize the angle)
+
+      % TODO: update the mean and covariance of the EKF for this landmark
+
+      % TODO: compute the likelihood of this observation, multiply with the former weight
+      %       to account for observing several features in one time step
+
+    end
+
+  end % measurement loop
+end % particle loop
+
+end
