05ae23c62bd9fba0c55d241e6cfc5fc53c1c0ab4
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2020-08-12 16:43:40 +0100

    :sparkles: Innit of assignment 9 of Robot Mapping Course
    Topic: Least squares and odometry calibration

---
+++ s9_odom_calib/octave/ls_calibrate_odometry.m
@@ -0,0 +1,40 @@
+% this function solves the odometry calibration problem
+% given a measurement matrix Z.
+% We assume that the information matrix is the identity
+% for each of the measurements
+% Every row of the matrix contains
+% z_i = [u'x, u'y, u'theta, ux, uy, ytheta]
+% Z:	The measurement matrix
+% X:	the calibration matrix
+% returns the correction matrix X
+function X = ls_calibrate_odometry(Z)
+  % initial solution (the identity transformation)
+  X = eye(3); 
+
+  % TODO: initialize H and b of the linear system
+  
+  % TODO: loop through the measurements and update H and b
+  % You may call the functions error_function and jacobian, see below
+  % We assume that the information matrix is the identity.
+
+  % TODO: solve and update the solution
+end
+
+% this function computes the error of the i^th measurement in Z
+% given the calibration parameters
+% i:	the number of the measurement
+% X:	the actual calibration parameters
+% Z:	the measurement matrix, each row contains first the scan-match result
+%       and then the motion reported by odometry
+% e:	the error of the ith measurement
+function e = error_function(i, X, Z)
+  % TODO compute the error of each measurement
+end
+
+% derivative of the error function for the ith measurement in Z
+% i:	the measurement number
+% Z:	the measurement matrix
+% J:	the jacobian of the ith measurement
+function J = jacobian(i, Z)
+  % TODO compute the Jacobian
+end
